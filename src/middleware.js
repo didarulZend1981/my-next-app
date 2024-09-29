c93285e27c09002b574d4888e32c08d2b59c9333
didarulZend1981exitgit config --global user.name didarulZend1981git config --global user.name didarulZend1981 <didarul1981@gmail.com>
2024-09-29 15:32:39 +0600

    middleware multiple route redirect

--- src/middleware.js
+++ src/middleware.js
@@ -1,9 +1,9 @@
 import { NextResponse } from 'next/server';
 
 export const middleware = (request) => {
-   return NextResponse.rewrite(new URL('/about', request.nextUrl.origin));
+   return NextResponse.redirect(new URL('/about', request.nextUrl.origin));
 };
 
 export const config = {
-  matcher: '/dashboards',
+  matcher: ['/dashboards','/services']
 };
